6d76038f47c82586f83091cf205e5e79607d4761
João Victor da Silva <joaovictordasilva0911@gmail.com>
2025-07-07 11:24:10 -0300

    Add Category component and styles for category selection

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,6 +2,7 @@ import { Image, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import {MaterialIcons} from '@expo/vector-icons'
 import { colors } from "@/styles/colors";
+import { Category } from "@/components/category";
 
 export default function Index() {
   return (
@@ -13,6 +14,10 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+
+      <Category name="Projeto" icon="code" isSelected />
+      <Category name="Site" icon="language" isSelected={false} />
+      <Category name="Video" icon="movie" isSelected={false} />
     </View>
   )
 }
---
+++ src/components/category/index.tsx
@@ -0,0 +1,22 @@
+import { colors } from "@/styles/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Pressable, Text, type PressableProps } from "react-native";
+import { styles } from "./styles";
+
+type Props = PressableProps & {
+  name: string
+  isSelected: boolean
+  icon: keyof typeof MaterialIcons.glyphMap
+}
+
+export function Category({ icon, name, isSelected, ...rest }: Props) {
+  const color = isSelected ? colors.green[300] : colors.gray[400]
+  
+  return (
+    <Pressable style={styles.container} {...rest}>
+      <MaterialIcons name={icon} size={16} color={color} />
+
+      <Text style={[styles.name, { color }]}>{name}</Text>
+    </Pressable>
+  )
+}
